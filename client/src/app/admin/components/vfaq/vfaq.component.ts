c2625298dfc259b57c80aeac158a926cffdd43f3
vivek singh <cu.vivek.1557@gmail.com>
2023-09-27 16:47:04 +0530

    faq to vfaq in admin module

--- client/src/app/admin/components/vfaq/vfaq.component.ts
+++ client/src/app/admin/components/vfaq/vfaq.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Renderer2,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-vfaq',
   templateUrl: './vfaq.component.html',
   styleUrls: ['./vfaq.component.scss']
 })
-export class VfaqComponent {
+export class VfaqComponent implements AfterViewInit {
+  @ViewChild('accordion', { static: false }) accordion!: ElementRef;
 
+  constructor(private renderer: Renderer2) {}
+
+  ngAfterViewInit(): void {
+    const items = this.accordion.nativeElement.querySelectorAll(
+      '.accordion .accordion-item'
+    );
+
+    const toggleAccordion = (event: any): void => {
+      const item = event.currentTarget;
+      const button = item.querySelector('button');
+      const isExpanded = button.getAttribute('aria-expanded') === 'true';
+
+      items.forEach((accordionItem: any) => {
+        this.renderer.setAttribute(
+          accordionItem.querySelector('button'),
+          'aria-expanded',
+          'false'
+        );
+      });
+
+      this.renderer.setAttribute(
+        button,
+        'aria-expanded',
+        isExpanded ? 'false' : 'true'
+      );
+    };
+
+    items.forEach((item: any) => {
+      this.renderer.listen(item, 'click', toggleAccordion);
+    });
+  }
 }
