ba22c3276116f5c50d8b6e0fb466e92ac27bf63c
vivek singh <cu.vivek.1557@gmail.com>
2023-09-29 13:14:02 +0530

    styling in profile page

--- client/src/app/admin/components/profile/profile.component.ts
+++ client/src/app/admin/components/profile/profile.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent {
+  HomeColor = '#ecc08d';
+  WorkColor = 'white';
+  isDisabled = true;
+  changeProfileBtn = 'Update Profile';
+  homeBtn() {
+    this.HomeColor = '#ecc08d';
+    this.WorkColor = 'white';
+    return false;
+  }
+  workBtn() {
+    this.WorkColor = '#ecc08d';
+    this.HomeColor = 'white';
+    return false;
+  }
 
+  prevent() {
+    this.isDisabled == true
+      ? (this.isDisabled = false)
+      : (this.isDisabled = true);
+
+    this.changeProfileBtn == 'Update Profile'
+      ? (this.changeProfileBtn = 'Save Changes')
+      : (this.changeProfileBtn = 'Update Profile');
+    return false;
+  }
 }
